3e058f4e6354c83ae154e8ede47599874fee13d3
imbzd <luochuan1234>
2015-09-16 23:52:17 +0800

    修复parse_name错误

--- AnyPHP/Common/functions.php
+++ AnyPHP/Common/functions.php
@@ -446,7 +446,8 @@ function parse_name($name, $type=0) {
     if ($type) {
         return ucfirst(preg_replace_callback('/_([a-zA-Z])/', function($match){return strtoupper($match[1]);}, $name));
     } else {
-        return strtolower(trim(preg_replace("/[A-Z]/", "_\\0", $name), "_"));
+        // return strtolower(trim(preg_replace("/[A-Z]/", "_\\0", $name), "_"));
+        return strtolower(trim(preg_replace("/[A-Z]/", "\\0", $name), "_"));
     }
 }
 
